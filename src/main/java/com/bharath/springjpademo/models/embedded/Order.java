28956f75b80a071b1a010fce1c953d4211e57e6e
bharaths44 <bharathsatheesan@gmail.com>
2025-09-03 21:24:22 +0530

    Added embedded ID

---
+++ src/main/java/com/bharath/springjpademo/models/embedded/Order.java
@@ -0,0 +1,22 @@
+package com.bharath.springjpademo.models.embedded;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="orders")
+public class Order {
+
+	@EmbeddedId
+	private OrderId id;
+	private String orderInfo;
+	private String anotherField;
+}
